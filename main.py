97957aec12b74bb99e9a3282e34be58db7dfae07
Egor <47773467+Crocussys@users.noreply.github.com>
2022-12-25 00:02:33 +0300

    Testing

--- main.py
+++ main.py
@@ -21,6 +21,76 @@ def approximation(x, y, x_all):
     return list(map(lambda num: a[0] + a[1] * num + a[2] * num ** 2, x_all))
 
 
+def spline(x, y, x_all):
+    n = len(x) - 1
+    a = x[0]
+    b = x[n]
+    h = (b - a) / n
+    ans = []
+    ms = [(4 * y[1] - y[2] - 3 * y[0]) / (2 * h)]
+    for i in range(1, n):
+        ms.append((y[i + 1] - y[i - 1]) / (2 * h))
+    ms.append((3 * y[n] - y[n - 2] - 3 * y[n - 1]) / (2 * h))
+    for i in range(n):
+        x_slice = []
+        for j in range(len(x_all)):
+            if i + 1 < n:
+                if x[i] <= x_all[j] < x[i + 1]:
+                    x_slice.append(x_all[j])
+            else:
+                if x[i] <= x_all[j] <= x[i + 1]:
+                    x_slice.append(x_all[j])
+        temp = list(map(lambda s: (((x[i + 1] - s) ** 2) * (2 * (s - x[i]) + h)) / (h ** 3) * y[i] +
+                                  (((s - x[i]) ** 2) * (2 * (x[i + 1] - s) + h)) / (h ** 3) * y[i + 1] +
+                                  (((x[i + 1] - s) ** 2) * (s - x[i])) / (h ** 2) * ms[i] +
+                                  (((s - x[i]) ** 2) * (s - x[i + 1])) / (h ** 2) * ms[i + 1], x_slice))
+        for j in range(len(temp)):
+            ans.append(temp[j])
+    return ans
+
+
+def differentiation_1(x, y):
+    ans = []
+    for i in range(len(x) - 1):
+        ans.append((y[i + 1] - y[i]) / (x[i + 1] - x[i]))
+    return ans
+
+
+def differentiation_2(x, y):
+    ans = []
+    for i in range(1, len(x)):
+        ans.append((y[i] - y[i - 1]) / (x[i] - x[i - 1]))
+    return ans
+
+
+def differentiation_3(x, y):
+    ans = []
+    for i in range(1, len(x) - 1):
+        ans.append((y[i + 1] - y[i - 1]) / (x[i + 1] - x[i - 1]))
+    return ans
+
+
+def integration_left(x, y):
+    ans = []
+    for i in range(len(x) - 1):
+        ans.append(y[i] * (x[i + 1] - x[i]))
+    return ans
+
+
+def integration_right(x, y):
+    ans = []
+    for i in range(len(x) - 1):
+        ans.append(y[i + 1] * (x[i + 1] - x[i]))
+    return ans
+
+
+def integration_middle(x, y):
+    ans = []
+    for i in range(len(x) - 1):
+        ans.append((y[i] + y[i + 1]) / 2 * (x[i + 1] - x[i]))
+    return ans
+
+
 def main():
     pit = open("pit.txt")
     y_values = list(map(int, pit.read().split(" ")))
@@ -31,22 +101,36 @@ def main():
     # y_interpolation_nodes = [174, 8, 174]
     # x_interpolation_nodes = [0, 204, 280, 136, 66]
     # y_interpolation_nodes = [174, 8, 174, 72, 116]
-    # x_approximation_nodes = [27, 54, 81, 108, 135]
-    # y_approximation_nodes = [90, 43.2, 40.32, 81, 161.28]
-    x_approximation_nodes = [x_pit_values[i * 15] for i in range(10)]
-    y_approximation_nodes = [y_values[i * 15] for i in range(10)]
     #
     # y_values_2 = list(map(formula_3p, x_all_values))
     # y_values_3 = list(map(formula_5p, x_all_values))
-    y_values_4 = approximation(np.array(x_approximation_nodes), np.array(y_approximation_nodes), x_all_values)
+    # y_values_4 = approximation(x_interpolation_nodes, y_interpolation_nodes, x_all_values)
+    # x_nodes = [x_pit_values[i] for i in range(len(x_pit_values)) if i % (len(x_pit_values) // 28) == 0]
+    # y_nodes = [y_values[i] for i in range(len(x_pit_values)) if i % (len(x_pit_values) // 28) == 0]
+    # y_values_5 = spline(x_nodes, y_nodes, x_all_values)
+    # y_values_6 = differentiation_1(x_pit_values, y_values)
+    # y_values_7 = differentiation_2(x_pit_values, y_values)
+    # y_values_8 = differentiation_3(x_pit_values, y_values)
+    # y_values_9 = differentiation_3(x_pit_values[1:-1], y_values_8)
+    y_values_10 = integration_left(x_pit_values, y_values)
+    y_values_11 = integration_right(x_pit_values, y_values)
+    y_values_12 = integration_right(x_pit_values, y_values)
 
     fig, ax = plt.subplots()
     # ax.scatter(x_interpolation_nodes, y_interpolation_nodes, label="Узлы интерполяции", color='red')
-    ax.scatter(x_approximation_nodes, y_approximation_nodes, label="Узлы аппроксимации", color='red')
-    ax.scatter(x_pit_values, y_values, label="Ручная оцифровка", s=10, color="purple")
+    # ax.scatter(x_nodes, y_nodes, label="Узлы аппроксимации", color='red')
+    # ax.scatter(x_pit_values, y_values, label="Ручная оцифровка", s=10, color="purple")
     # ax.plot(x_all_values, y_values_2, label="Интерполяция")
     # ax.plot(x_all_values, y_values_3, label="Интерполяция")
-    ax.plot(x_all_values, y_values_4, label="Аппроксимация")
+    # ax.plot(x_all_values, y_values_4, label="Аппроксимация")
+    # ax.plot(x_all_values, y_values_5, label="Сплайн")
+    # ax.plot(x_pit_values[:-1], y_values_6, label="Дифференцирование 1")
+    # ax.plot(x_pit_values[1:], y_values_7, label="Дифференцирование 2")
+    # ax.plot(x_pit_values[1:-1], y_values_8, label="Дифференцирование 3")
+    # ax.plot(x_pit_values[2:-2], y_values_9, label="Дифференцирование x2")
+    ax.plot(x_pit_values[:-1], y_values_10, label="Интегрирование левыми")
+    ax.plot(x_pit_values[:-1], y_values_11, label="Интегрирование правыми")
+    ax.plot(x_pit_values[:-1], y_values_12, label="Интегрирование средними")
 
     ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
     ax.xaxis.set_minor_locator(ticker.MultipleLocator(5))
@@ -56,7 +140,7 @@ def main():
     ax.grid(which='minor', color='gray', linestyle=':')
     ax.legend()
     plt.xlim([0, 280])
-    plt.ylim([0, 180])
+    # plt.ylim([0, 180])
     # plt.title("Отцифрованная ямка", fontsize=14)
     plt.xlabel("X мм", fontsize=14)
     plt.ylabel("Y мм", fontsize=14)
