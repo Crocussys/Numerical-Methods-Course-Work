b00fce181092c3aceebee82144088a43d0053818
Egor <47773467+Crocussys@users.noreply.github.com>
2022-12-27 01:36:19 +0300

    Туда-сюда

--- animation.py
+++ animation.py
@@ -3,12 +3,15 @@ import matplotlib.ticker as ticker
 import matplotlib.animation as animation
 import numpy as np
 
+g = 10
+m = 1
+fps = 300
+nu = 0
+
 
 def spline(x_all):
     len_nodes = len(x_nodes) - 1
-    a = x_nodes[0]
-    b = x_nodes[len_nodes]
-    h = (b - a) / len_nodes
+    h = (x_nodes[len_nodes] - x_nodes[0]) / len_nodes
     ans = []
     ms = [(4 * y_nodes[1] - y_nodes[2] - 3 * y_nodes[0]) / (2 * h)]
     for i in range(1, len_nodes):
@@ -33,8 +36,22 @@ def spline(x_all):
     return ans
 
 
+def differentiation_in_point(xs, ys, point):
+    for i in range(1, len(xs)):
+        if point - xs[i] < 0:
+            return (ys[i] - ys[i - 1]) / (xs[i] - xs[i - 1])
+    raise
+
+
+def sign(elem):
+    if elem >= 0:
+        return -1
+    else:
+        return 1
+
+
 def animate(i):
-    scat.set_offsets((t[i], spline([t[i]])[0]))
+    scat.set_offsets(frames[i])
     return scat,
 
 
@@ -50,13 +67,13 @@ for i in range(n):
         x_nodes.append(x_pit_values[i])
         y_nodes.append(y_values_pit[i])
 
-fig, ax = plt.subplots()
-ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
-ax.xaxis.set_minor_locator(ticker.MultipleLocator(5))
-ax.yaxis.set_major_locator(ticker.MultipleLocator(10))
-ax.yaxis.set_minor_locator(ticker.MultipleLocator(5))
-ax.grid(which='major', color='gray')
-ax.grid(which='minor', color='gray', linestyle=':')
+fig, axis = plt.subplots()
+axis.xaxis.set_major_locator(ticker.MultipleLocator(10))
+axis.xaxis.set_minor_locator(ticker.MultipleLocator(5))
+axis.yaxis.set_major_locator(ticker.MultipleLocator(10))
+axis.yaxis.set_minor_locator(ticker.MultipleLocator(5))
+axis.grid(which='major', color='gray')
+axis.grid(which='minor', color='gray', linestyle=':')
 # ax.legend()
 plt.xlim([0, 280])
 plt.ylim([0, 180])
@@ -64,10 +81,42 @@ plt.xlabel("X мм", fontsize=14)
 plt.ylabel("Y мм", fontsize=14)
 
 y_values = spline(x_all_values)
-ax.plot(x_all_values, y_values, label="Траектория")
+axis.plot(x_all_values, y_values, label="Траектория")
 x = int(input("Введите начальное положение "))
-scat = ax.scatter(x, spline([x])[0])
-t = np.linspace(0., 280., num=280)
+v = int(input("Введите начальную скорость "))
+tg_a = differentiation_in_point(x_all_values, y_values, x)
+cos_a = np.sqrt(1 / (1 + tg_a ** 2))
+vx = - v * cos_a
+vy = v * tg_a * cos_a
+y0 = spline([x])[0]
+energy = m * g * y0
+ax = -1 * g * (1 / tg_a)
+t = 1 / fps
+frames = [(x, y0)]
+x_new = x + vx * t + (ax * t ** 2) / 2
+y_prev = y0
+while np.abs(x_new - x) > 0.000001 and 0 <= x_new <= 280:
+    x = x_new
+    y = spline([x])[0]
+    frames.append((x, y))
+    tg_a = differentiation_in_point(x_all_values, y_values, x)
+    vy_new = np.sqrt(2 * (energy / m - g * y))
+    ay = (vy_new - vy) / t
+    if x_new - x >= 0 and y_prev - y >= 0:
+        ax = (g + ay) * ((nu + 1) / (nu - 1)) * (1 / tg_a)
+    elif x_new - x < 0 and y_prev - y < 0:
+        nu_coef = ((nu - 1) / (nu + 1))
+        ax = (g + ay) * ((nu + 1) / (nu - 1)) * (1 / tg_a)
+    elif x_new - x >= 0 and y_prev - y < 0:
+        ax = (ay - g) * ((nu + 1) / (nu - 1)) * tg_a
+    else:
+        ax = (ay - g) * ((nu + 1) / (nu - 1)) * tg_a
+    # ax = sign(tg_a) * (g + ay) * ((nu + 1) / (nu - 1)) * (1 / tg_a)
+    vx += ax * t
+    x_new = x + vx * t + (ax * t ** 2) / 2
+    y_prev = y
+    print(f"x = {x_new} tg_a = {tg_a} vy = {vy_new} ay = {ay} ax = {ax} vx = {vx} y = {y}")
 
-ani = animation.FuncAnimation(fig, animate, repeat=False, frames=len(t) - 1, interval=0)
+scat = axis.scatter([], [])
+ani = animation.FuncAnimation(fig, animate, repeat=True, frames=len(frames) - 1, interval=1000/fps, blit=True)
 plt.show()
